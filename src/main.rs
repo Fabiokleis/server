a59fe3e4df475dddd1d20a4c8894b6a095137176
fabiokleis <fabiohkrc@gmail.com>
2022-07-09 01:19:56 -0300

    created a simple server that handle request and send html index and 404 error page

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::fs;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0u8; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap(); 
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        handle_connection(stream);
     }
-    println!("Connection established!");
 }
